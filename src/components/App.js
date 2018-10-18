1c7c146f3ea4d0569d8d2058375027ab3b0154a0
Dev-JeromeBaek <superior051@icloud.com>
2018-10-19 02:04:24 +0900

    docs: A new structure of the rework skeleton project
    1. repository 변경
    
    2. react-skeleton-babel7 버전 적용

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Test from './Test';
+import '../assets/css/style.css';
+import image from '../assets/images/image.png';
+
+class App extends Component {
+  render() {
+    const title = 'Hello React Babel7 Skeleton';
+    return (
+      <div>
+        <h1 className="hello">{title}</h1>
+        <img src={image} />
+        <Test />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]',
+          // name: '[name].[ext]',
+          // outputPath: './images/',
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]',
+          // outputPath: './fonts/',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/awesome-react-babel7-skeleton/',
+    filename: 'static/js/bundle.js',
+  },
+  devServer: {
+    host: '0.0.0.0',
+    contentBase: './dist',
+    compress: true,
+    hot: true,
+    inline: true,
+    port: 9009,
+    open: true,
+    stats: {
+      assets: false,
+      colors: true,
+      version: false,
+      timings: false,
+      chunks: false,
+      chunkModules: false,
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: './public/favicon.ico',
+      template: './public/index.html',
+    }),
+    new UglifyJSPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
